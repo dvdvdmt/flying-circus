32f7cc54b0733f7a86f42373ef64b5f31b60cfb5
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-03-20 17:41:03 +0500

    Add prettier and remove eslint

--- src/merge-sort/index.ts
+++ src/merge-sort/index.ts
@@ -1,10 +1,9 @@
 /* eslint-disable no-await-in-loop, max-len */
-import initPlot from './plot';
-import encodeImage, {Image} from './encode-image';
-
-// const array = range(30);
+import initPlot from './plot'
+import encodeImage, {Image} from './encode-image'
 
 const image: Image = {
+  // prettier-ignore
   data: [
     [1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1],
     [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
@@ -14,87 +13,89 @@ const image: Image = {
   ],
   bottomPadding: 3,
   rightPadding: 3,
-};
-// const image: Image = {
-//   data: [
-//     [1, 1],
-//     [0, 1],
-//   ],
-//   bottomPadding: 0,
-//   rightPadding: 0,
-// };
+}
 
 function generateArray(): number[] {
-  return Array.from(new Array(50), () => Math.random());
+  return Array.from(new Array(50), () => Math.random())
 }
 
 window.addEventListener('load', () => {
-  const chartEl = document.querySelector('.chart') as SVGElement;
-  const chartWidth = Number.parseInt(getComputedStyle(chartEl).width, 10);
-  const chartHeight = chartWidth;
-  chartEl.style.height = `${chartHeight}px`;
-  const plot = initPlot(chartEl, chartWidth, chartHeight);
-  let array: number[] = generateArray();
-  let state = [...array];
-  let encodedImage = encodeImage([...array], image);
-  const runBtnEl = document.querySelector('.app__button') as HTMLButtonElement;
-  plot({data: state, encodedImage});
-  let isFirstRun = true;
+  const chartEl = document.querySelector('.chart') as SVGElement
+  const chartWidth = Number.parseInt(getComputedStyle(chartEl).width!, 10)
+  const chartHeight = chartWidth
+  chartEl.style.height = `${chartHeight}px`
+  const plot = initPlot(chartEl, chartWidth, chartHeight)
+  let array: number[] = generateArray()
+  let state = [...array]
+  let encodedImage = encodeImage([...array], image)
+  const runBtnEl = document.querySelector('.app__button') as HTMLButtonElement
+  plot({data: state, encodedImage})
+  let isFirstRun = true
   runBtnEl.addEventListener('click', () => {
-    runBtnEl.disabled = true;
+    runBtnEl.disabled = true
     if (!isFirstRun) {
-      array = generateArray();
-      encodedImage = encodeImage([...array], image);
-      state = [...array];
+      array = generateArray()
+      encodedImage = encodeImage([...array], image)
+      state = [...array]
     }
     mergeSort(array).then(() => {
-      runBtnEl.disabled = false;
-      isFirstRun = false;
-    });
-  });
+      runBtnEl.disabled = false
+      isFirstRun = false
+    })
+  })
 
   async function mergeSort(arr: number[], offset = 0): Promise<number[]> {
     if (arr.length <= 1) {
-      return arr;
+      return arr
     }
-    const middle = Math.floor(arr.length / 2);
-    const left = await mergeSort(arr.slice(0, middle), offset);
-    const right = await mergeSort(arr.slice(middle), offset + middle);
-    return merge(left, right, offset);
+    const middle = Math.floor(arr.length / 2)
+    const left = await mergeSort(arr.slice(0, middle), offset)
+    const right = await mergeSort(arr.slice(middle), offset + middle)
+    return merge(left, right, offset)
   }
 
-  async function merge(left: number[], right: number[], offset = 0): Promise<number[]> {
-    const result = [];
-    let l = 0;
-    let r = 0;
+  async function merge(
+    left: number[],
+    right: number[],
+    offset = 0
+  ): Promise<number[]> {
+    const result = []
+    let l = 0
+    let r = 0
     while (l < left.length && r < right.length) {
-      const firstL = left[l];
-      const firstR = right[r];
+      const firstL = left[l]
+      const firstR = right[r]
       if (firstL <= firstR) {
-        result.push(firstL);
-        l += 1;
+        result.push(firstL)
+        l += 1
       } else {
-        result.push(firstR);
-        r += 1;
+        result.push(firstR)
+        r += 1
       }
-      await updateState([...result, ...left.slice(l), ...right.slice(r)], offset);
+      await updateState(
+        [...result, ...left.slice(l), ...right.slice(r)],
+        offset
+      )
     }
     if (left.length) {
-      result.push(...left.slice(l));
+      result.push(...left.slice(l))
     }
     if (right.length) {
-      result.push(...right.slice(r));
+      result.push(...right.slice(r))
     }
-    return result;
+    return result
   }
 
-  async function updateState(part: number[], start: number): Promise<undefined> {
-    state.splice(start, part.length, ...part);
-    plot({data: state, mergeRange: [start, start + part.length], encodedImage});
+  async function updateState(
+    part: number[],
+    start: number
+  ): Promise<undefined> {
+    state.splice(start, part.length, ...part)
+    plot({data: state, mergeRange: [start, start + part.length], encodedImage})
     return new Promise((resolve) => {
       requestAnimationFrame(() => {
-        resolve();
-      });
-    });
+        resolve()
+      })
+    })
   }
-});
+})
--- src/merge-sort/plot.ts
+++ src/merge-sort/plot.ts
@@ -1,63 +1,81 @@
-import c from 'classnames';
-import * as d3 from 'd3';
-import {EncodedImage} from './encode-image';
+import c from 'classnames'
+import * as d3 from 'd3'
+import {EncodedImage} from './encode-image'
 
 interface PlotInterface {
-  data: number[];
-  mergeRange?: [number, number];
-  encodedImage: EncodedImage;
+  data: number[]
+  mergeRange?: [number, number]
+  encodedImage: EncodedImage
 }
 
-export default function initPlot(chartEl, chartWidth, chartHeight) {
+export default function initPlot(
+  chartEl: SVGElement,
+  chartWidth: number,
+  chartHeight: number
+) {
   return function plot({
     data,
     mergeRange = [0, data.length],
     encodedImage,
   }: PlotInterface): void {
-    const barWidth = chartWidth / data.length;
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(data)])
-      .rangeRound([chartHeight, 0]);
+    const barWidth = chartWidth / data.length
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data)!])
+      .rangeRound([chartHeight, 0])
 
-    const bar = d3.select(chartEl).selectAll('g').data(data);
-    bar.selectAll('rect').remove();
-    initBarContainers(bar);
-    initBars(bar);
+    const bar = d3.select(chartEl).selectAll('g').data(data)
+    bar.selectAll('rect').remove()
+    initBarContainers(bar)
+    initBars(bar)
 
-    const barEnter = bar.enter().append('g');
-    initBarContainers(barEnter);
-    initBars(barEnter);
+    const barEnter = bar.enter().append('g')
+    initBarContainers(barEnter)
+    initBars(barEnter)
 
-    bar.exit().remove();
+    bar.exit().remove()
 
-    function initBarContainers(selection): void {
-      selection.attr('transform', (_, i) => `translate(${i * barWidth},0)`);
+    function initBarContainers(
+      selection:
+        | d3.Selection<d3.BaseType, number, SVGElement, unknown>
+        | d3.Selection<SVGGElement, number, SVGElement, unknown>
+    ): void {
+      selection.attr('transform', (_, i) => `translate(${i * barWidth},0)`)
     }
 
-    function initBars(selection): void {
-      selection.append('rect')
-        .attr('class', (_, i) => c('chart__bar', {'chart__bar--colored': isInMergeRange(i)}))
+    function initBars(
+      selection:
+        | d3.Selection<d3.BaseType, number, SVGElement, unknown>
+        | d3.Selection<SVGGElement, number, SVGElement, unknown>
+    ): void {
+      selection
+        .append('rect')
+        .attr('class', (_, i) =>
+          c('chart__bar', {'chart__bar--colored': isInMergeRange(i)})
+        )
         .attr('width', barWidth)
-        .attr('height', (v) => (chartHeight - yScale(v)))
-        .attr('y', yScale);
+        .attr('height', (v) => chartHeight - yScale(v))
+        .attr('y', yScale)
 
       if (encodedImage) {
-        const barsWithMarks = selection.filter((d) => encodedImage[d]);
-        const marks = barsWithMarks.selectAll('.chart__mark')
-          .data((d) => encodedImage[d]);
+        const barsWithMarks = selection.filter((d) => encodedImage[d])
+        const marks = barsWithMarks
+          .selectAll('.chart__mark')
+          .data((d) => encodedImage[d])
 
-        marks.enter()
+        marks
+          .enter()
           .append('rect')
           .attr('class', (d) => c('chart__mark', {'chart__mark--visible': d}))
           .attr('width', barWidth)
           .attr('height', barWidth)
-          .attr('y', (_, i) => chartHeight - (i * barWidth) - barWidth);
+          .attr('y', (_, i) => chartHeight - i * barWidth - barWidth)
       }
     }
 
-    function isInMergeRange(x): boolean {
-      const [min, max] = mergeRange;
-      return x >= min && x <= max;
+    function isInMergeRange(x: number): boolean {
+      const [min, max] = mergeRange
+      return x >= min && x <= max
     }
-  };
+  }
 }
