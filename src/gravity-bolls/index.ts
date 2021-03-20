98de2b65bea60ef3e9b3c0ccfee3d22c49868d8f
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-03-20 23:40:51 +0500

    Draw circle shape

--- src/gravity-bolls/index.ts
+++ src/gravity-bolls/index.ts
@@ -2,7 +2,22 @@ import * as PIXI from 'pixi.js'
 
 document.addEventListener('DOMContentLoaded', main)
 
-function main() {
-  const app = new PIXI.Application({width: 256, height: 256})
+function main(): void {
+  const app = createApp()
+  const graphics = new PIXI.Graphics()
+  graphics.beginFill(0x0000ff)
+  graphics.drawCircle(0, 0, 250)
+  app.stage.addChild(graphics)
+}
+
+function createApp(): PIXI.Application {
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const app = new PIXI.Application({
+    width,
+    height,
+    autoDensity: true,
+  })
   document.querySelector('.app')!.appendChild(app.view)
+  return app
 }
