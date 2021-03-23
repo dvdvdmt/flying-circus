57895f0ca207d5cd9b8c8bbe9562361eb968cd1a
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-03-23 20:22:02 +0500

    Separate scene setup and solar system data

--- src/gravity-bolls/setup-scene.ts
+++ src/gravity-bolls/setup-scene.ts
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js'
-import {ICelestialBodyData} from './types'
 import {CelestialBody} from './celestial-body'
+import {solarSystemData} from './solar-system-data'
 
 export function setupScene(app: PIXI.Application): void {
   const sceneCenter: PIXI.IPointData = {
@@ -19,55 +19,4 @@ export function setupScene(app: PIXI.Application): void {
       planet.move()
     })
   })
-}
-
-function solarSystemData(center: PIXI.IPointData): ICelestialBodyData[] {
-  const orbitGap = 5
-  const sunRadius = 200
-  return [
-    {
-      name: 'Sun',
-      position: center,
-      gravityCenter: center,
-      radius: sunRadius,
-      color: 0xfdffa3,
-    },
-    {
-      name: 'Mercury',
-      position: center,
-      gravityCenter: center,
-      radius: 5,
-      color: 0xb1a7a2,
-    },
-    {
-      name: 'Venus',
-      position: center,
-      gravityCenter: center,
-      radius: 10,
-      color: 0xf3d39d,
-    },
-  ].map(alignBodies)
-
-  function alignBodies(
-    data: ICelestialBodyData,
-    i: number,
-    arr: ICelestialBodyData[]
-  ) {
-    if (i === 0) {
-      return data
-    }
-    const prevData = arr[i - 1]
-    data.position = nextPosition(prevData, data)
-    return data
-  }
-
-  function nextPosition(
-    prevBody: ICelestialBodyData,
-    nextBody: ICelestialBodyData
-  ) {
-    return {
-      x: nextBody.position.x,
-      y: prevBody.position.y - prevBody.radius - orbitGap - nextBody.radius,
-    }
-  }
 }
---
+++ src/gravity-bolls/solar-system-data.ts
@@ -0,0 +1,53 @@
+import * as PIXI from 'pixi.js'
+import {ICelestialBodyData} from './types'
+
+export function solarSystemData(center: PIXI.IPointData): ICelestialBodyData[] {
+  const orbitGap = 5
+  const sunRadius = 200
+  return [
+    {
+      name: 'Sun',
+      position: center,
+      gravityCenter: center,
+      radius: sunRadius,
+      color: 0xfdffa3,
+    },
+    {
+      name: 'Mercury',
+      position: center,
+      gravityCenter: center,
+      radius: 5,
+      color: 0xb1a7a2,
+    },
+    {
+      name: 'Venus',
+      position: center,
+      gravityCenter: center,
+      radius: 10,
+      color: 0xf3d39d,
+    },
+  ].map(alignBodies)
+
+  function alignBodies(
+    data: ICelestialBodyData,
+    i: number,
+    arr: ICelestialBodyData[]
+  ) {
+    if (i === 0) {
+      return data
+    }
+    const prevData = arr[i - 1]
+    data.position = nextPosition(prevData, data)
+    return data
+  }
+
+  function nextPosition(
+    prevBody: ICelestialBodyData,
+    nextBody: ICelestialBodyData
+  ) {
+    return {
+      x: nextBody.position.x,
+      y: prevBody.position.y - prevBody.radius - orbitGap - nextBody.radius,
+    }
+  }
+}
