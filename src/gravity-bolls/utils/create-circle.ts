872a6add2ba4271c564bd52930b2cba38ee2f2fb
Dmitriy Davydov <dvdvdmt@gmail.com>
2021-04-03 18:51:50 +0500

    End fill for circles

--- src/gravity-bolls/utils/create-circle.ts
+++ src/gravity-bolls/utils/create-circle.ts
@@ -7,5 +7,6 @@ export function createCircle(radius: number, color = 0x0000ff): PIXI.Graphics {
   // It is better to draw a circle in the center of coordinate system.
   // The placement of a child on a scene (parent) is regulated with its 'position' property.
   graphics.drawCircle(0, 0, radius)
+  graphics.endFill()
   return graphics
 }
